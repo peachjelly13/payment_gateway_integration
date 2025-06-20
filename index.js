cba408a37a37247990c7a6d375407d2359043a34
peachjelly13 <ananyasharma112233441234@gmail.com>
2025-06-20 20:40:37 +0530

    security measures added

--- index.js
+++ index.js
@@ -2,17 +2,49 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const { Payment, Callback } = require('ecommpay');
+const Joi = require('joi');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+
+
 
 
 
 const app = express();
 app.use(bodyParser.json());
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000,
+    max: 10,            // 10 requests per minute maximum     
+});
+app.use(helmet());
+app.use('/pay', limiter)
 
 const projectId = process.env.project_id;
 const secret_salt = process.env.secret_salt;
 
 
+if (!projectId || !secret_salt) {
+    console.error("Missing projectId or secret_salt in environment variables");
+    process.exit(1);
+}
+
+
 app.post('/pay', (req, res) => {
+    const schema = Joi.object({
+        phone: Joi.string().required(),
+        email: Joi.string().email().required(),
+        postal: Joi.string().required(),
+        country: Joi.string().length(2).required(),
+        city: Joi.string().required(),
+        address: Joi.string().required(),
+        amount: Joi.number().positive().required(),
+        currency: Joi.string().length(3).required(),
+        customerId: Joi.string().required(),
+        paymentId: Joi.string().required()
+    });
+
+    const { error } = schema.validate(req.body);
+    if (error) return res.status(400).send(error.message);
     // generate a unique paymentId expected 
     const {
         phone,
@@ -63,7 +95,7 @@ app.post('/payment/callback', (req, res) => {
 
             // if we want to do any database operations we can add them here 
             // mark order as paid, send email etc.
-            console.log('Payment Success:', paymentId, paymentData);
+            console.log('Payment Success:', paymentId, paymentData.status);
         }
         else if (paymentData.status === "decline" || paymentData.status === "fail") {
             // here we would want them to retry the payment 
@@ -87,3 +119,5 @@ const PORT = 8000;
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
+
+//do not log any sensitive information in production 
